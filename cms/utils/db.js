66585effa82ba8cc326d042ae5da142968098c50
Matthew Barker <matthewbarker@librem.one>
2026-01-25 09:10:41 +0000

    feat(cms): add database utility

---
+++ cms/utils/db.js
@@ -0,0 +1,13 @@
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+
+const dbPath = path.join(__dirname, '../db/mindfull.sqlite');
+const db = new sqlite3.Database(dbPath, (err) => {
+  if (err) {
+    console.error('Error connecting to database:', err.message);
+  } else {
+    console.log('Connected to the MindFull CMS database.');
+  }
+});
+
+module.exports = db;
