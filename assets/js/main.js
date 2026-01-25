9183f04163026320e0b31d4f27903e148533d29c
Matthew Barker <matthewbarker@librem.one>
2026-01-25 09:08:12 +0000

    style: fix trailing commas in assets/js/main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,7 +30,7 @@ window.showToast = function (message, type = 'info', duration = 5000) {
         'text-green-500',
         'bg-green-100',
         'dark:bg-green-800',
-        'dark:text-green-200'
+        'dark:text-green-200',
       );
       iconSvg =
         '<svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20"><path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/></svg>';
@@ -40,7 +40,7 @@ window.showToast = function (message, type = 'info', duration = 5000) {
         'text-red-500',
         'bg-red-100',
         'dark:bg-red-800',
-        'dark:text-red-200'
+        'dark:text-red-200',
       );
       iconSvg =
         '<svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20"><path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z"/></svg>';
@@ -50,7 +50,7 @@ window.showToast = function (message, type = 'info', duration = 5000) {
         'text-blue-500',
         'bg-blue-100',
         'dark:bg-blue-800',
-        'dark:text-blue-200'
+        'dark:text-blue-200',
       );
       iconSvg =
         '<svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20"><path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9 5h2v2H9V5Zm0 4h2v7H9V9Z"/></svg>';
